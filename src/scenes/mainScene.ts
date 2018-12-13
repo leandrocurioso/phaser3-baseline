bf5c65bea534e942017c7c776a61de160939f531
Leandro Curioso <leandro.curioso@gmail.com>
2018-12-13 12:41:25 -0200

    Added code

---
+++ src/scenes/mainScene.ts
@@ -0,0 +1,18 @@
+export class MainScene extends Phaser.Scene {
+  private phaserSprite: Phaser.GameObjects.Sprite;
+
+  constructor() {
+    super({
+      key: "MainScene"
+    });
+  }
+
+  preload(): void {
+    this.load.image("logo", "./assets/phaser.png");
+  }
+
+  create(): void {
+    this.phaserSprite = this.add.sprite(0, 0, "logo");
+    this.phaserSprite .setOrigin(0, 0);
+  }
+}
